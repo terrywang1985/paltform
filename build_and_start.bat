1537f955b9ea24166bc38c8757384fd20fffe036
terry <terry@diezhi.local>
2025-09-01 19:08:14 +0800

    go work

--- build_and_start.bat
+++ build_and_start.bat
@@ -1,7 +1,18 @@
+
+go work init
+
+go work use ./auth-service
+go work use ./user-service
+go work use ./payment-service
+go work use ./backstage-service
+go work use ./shared
+
+
 cd auth-service && go mod tidy && cd ..
 cd user-service && go mod tidy && cd ..
 cd payment-service && go mod tidy && cd ..
 cd backstage-service && go mod tidy && cd ..
 cd api-gateway && go mod tidy && cd ..
+cd shared && go mod tidy && cd ..
 
 docker-compose up -d --build
